f2e5af65f4e2eba337bb09468d89d9d37836aac4
Thiago Magno <thgmgn@gmail.com>
2025-04-29 18:23:26 -0300

    update api response

--- src/app/api/url/[id]/route.ts
+++ src/app/api/url/[id]/route.ts
@@ -2,18 +2,23 @@ import { redis } from '@/lib/redis'
 
 export async function GET(req: Request) {
   const id = req.url.split('/').pop()
+
   if (!id) {
     return Response.json({
-      error: true,
-      message: 'Invalid parameters',
+      status: 'erro',
+      message: 'Parâmetros inválidos.',
+      details: 'Certifique-se de fornecer um identificador válido na URL.',
     })
   }
 
   const originalUrl = await redis.get(`short:${id}`)
+
   if (!originalUrl) {
     return Response.json({
-      error: true,
-      message: 'URL not found',
+      status: 'erro',
+      message: 'URL não encontrada.',
+      details:
+        'Este projeto é parte de um portfólio pessoal e não possui fins comerciais. As URLs expiram em 30 dias. Caso a URL tenha expirado, gere uma nova.',
     })
   }
 
